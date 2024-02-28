3490c671f84feadfedc08c4223114a7b41b14e0c
Esrael Mekdem <esraelmekdem@gmail.com>
2024-02-28 16:13:44 +0300

    Update Band Name Generator.py

--- Band Name Generator.py
+++ Band Name Generator.py
@@ -1,4 +1,7 @@
 print("Welcome to the Band Name Generator.")
+# accepts the city where the user grew up in.
 city_name = input("What's the name of the city you grew up in?\n")
+# accepts what the users pet name is .
 pet_name = input("What's your pet's name?\n")
+# concats them together.
 print("Your band name could be"+ " " + city_name + " " + pet_name)

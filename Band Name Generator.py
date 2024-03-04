02072ae822cf6d0cbb093a753cc0a6cbc87104de
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-04 16:18:20 +0300

    added random name generator

--- Band Name Generator.py
+++ Band Name Generator.py
@@ -1,7 +1,16 @@
+import word_list
+import random
 print("Welcome to the Band Name Generator.")
 # accepts the city where the user grew up in.
-city_name = input("What's the name of the city you grew up in?\n")
+city_name = list(input("What's the name of the city you grew up in?\n"))
 # accepts what the users pet name is .
 pet_name = input("What's your pet's name?\n")
-# concats them together.
-print("Your band name could be"+ " " + city_name + " " + pet_name)
+
+def namer(name):
+    name = name[0]
+    word = word_list.word_dict[name][random.randint(0,25)]
+    return word
+
+band_name = f"Your band name should be '{namer(city_name)} {namer(pet_name)}'"
+
+print(band_name)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+import word_list
+import random
+print("Welcome to the Band Name Generator.")
+# accepts the city where the user grew up in.
+city_name = input("What's the name of the city you grew up in?\n")
+# accepts what the users pet name is .
+pet_name = input("What's your pet's name?\n")
+city_name = city_name.strip()
+print(city_name)
